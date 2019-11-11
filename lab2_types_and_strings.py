379de71565443c6e6bb4e633f24d297636b68bcc
David Tilson <davidrtilson@gmail.com>
2019-11-11 15:46:54 +0000

    lab1 fix

--- lab2_types_and_strings.py
+++ lab2_types_and_strings.py
@@ -2,7 +2,7 @@
 #academic year: 201920
 #author: B. Schoen-Phelan
 #date: 29-09-2019
-#purpose: Lab 2
+#purpose: Lab 2 Solution
 
 class Types_and_Strings:
     def __init__(self):
@@ -14,49 +14,64 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # print only first and last of the sentence
-
+        print("First character: " + message[0])
+        print("Last character : " + message[-1])
 
         # use slice notation
-
+        print("Print from position 3 to end: " + message[3:])
+        print("Print up to prosition 3: " + message[:3])
+        print("Prints everthing via slice: " + message[:])
 
         # escaping a character
-
+        print("He said \"that\'s fantastic\"!")
 
         # find all a's in the input word and count how many there are
-
+        lower_message = message.lower()
+        print("all lower characters: " +lower_message)
+        print("The first occurence of a is at position: " +str(lower_message.find('a')))
+        print("There are "+ str(lower_message.count('a'))+ " a's in the word.")
+        print("Total character count is: " +str(len(lower_message)))
 
         # replace all occurences of the character a with the - sign
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace()
-
+        print(lower_message.replace('a', '-'))
 
         # printing only characters at even index positions
-
+        for i in range(0, len(message) - 1, 2):
+            print("index[", i, "]", message[i])
 
     def play_with_lists(self):
         message = input("Please enter a whole sentence: ")
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out
-
+        li = list(message.split(" "))
+        print(li)
 
         # append a new element to the list and print
-
+        li.append("new")
+        print(li)
 
         # remove from the list in 3 ways
-
+        print(li.pop())
+        print(li.remove("cake"))
+        del li[-1:-2]
+        print(li)
 
         # check if the word cake is in your input list
-
+        print('cake' in li)
 
         # reverse the items in the list and print
-
+        li.reverse()
+        print(li)
 
         # reverse the list with the slicing trick
-
+        li[::-1]
+        print(li)
 
         # print the list 3 times by using multiplication
-
+        print(li*3)
 
 
 tas = Types_and_Strings()
